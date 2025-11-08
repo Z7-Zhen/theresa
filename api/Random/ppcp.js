5eebb25287ebf885921989e913814d036863f307
Z7-zhen <ha3590962@gmai.com>
2025-11-08 09:36:23 +0000

    update: push terbaru tanpa node_modules & package-lock.json

---
+++ api/Random/ppcp.js
@@ -0,0 +1,48 @@
+mport axios from 'axios'
+
+async function getPPCouple() {
+  try {
+    const { data } = await axios.get('https://raw.githubusercontent.com/KazukoGans/database/main/anime/ppcouple.json')
+    const random = data[Math.floor(Math.random() * data.length)]
+    return {
+      male: random.cowo,
+      female: random.cewe
+    }
+  } catch (error) {
+    console.error("Error fetching couple data:", error)
+    throw new Error("Gagal mengambil data PP Couple.")
+  }
+}
+
+export default {
+  name: "PP Couple",
+  desc: "Menampilkan foto profil pasangan anime (cowok dan cewek).",
+  category: "Random",
+  path: "/random/ppcouple?apikey=",
+  async run(req, res) {
+    const { apikey } = req.query
+
+    // Validasi API key
+    if (!apikey || !global.apikey?.includes(apikey)) {
+      return res.status(403).json({
+        status: false,
+        message: "Apikey tidak valid!",
+      })
+    }
+
+    try {
+      const result = await getPPCouple()
+
+      res.status(200).json({
+        status: true,
+        creator: "Z7:林企业",
+        result
+      })
+    } catch (error) {
+      res.status(500).json({
+        status: false,
+        message: error.message,
+      })
+    }
+  },
+}
